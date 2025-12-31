6068f88bc8389d14265f12463adf7af35c1039f7
Cuite Piglin <65344456+cmy2008@users.noreply.github.com>
2026-01-01 01:22:35 +0800

    getpass2

--- auto_attend.py
+++ auto_attend.py
@@ -1,13 +1,24 @@
+import sys
+sys.path.append('.')
 from yunban import *
 
-test=yunban("", "")
+test=yunban("")
 classlist=test.getclasslist()
 students=test.getstulist(classlist[35]["uid"])
 events=test.getevents(classlist[35]["roomUid"])
-namelist=[]
-for name in namelist:
-    stu=test.serchstubyname(name,students)
-    faketime=test.randomtime(events[2])
+uidlist=[]
+for stu in students:
+    # stu=test.serchstubyuid(uid,students)
+    try:
+        if sys.argv[1]:
+            event=events[int(sys.argv[1])]
+        else:
+            event=events[1]
+    except:
+        event=events[1]
+    # print(sys.argv[1])
+    faketime=test.randomtime(event)
     datenow=time.strftime('%Y-%m-%d', time.localtime())
-    print(test.attend(stu["name"],stu["uid"],stu["sid"],events[1],datenow,faketime,classlist[35]["uid"],classlist[35]["roomUid"]))
-    print(f"{name}已签到，时间：{datenow} {faketime}")
+    timenow=time.strftime('%H:%M:%S', time.localtime())
+    print(test.attend(stu["name"],stu["uid"],stu["sid"],event,datenow,faketime,classlist[35]["uid"],classlist[35]["roomUid"]))
+    print(f"{stu["name"]}已签到，时间：{datenow} {faketime}")
